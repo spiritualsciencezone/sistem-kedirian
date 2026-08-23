03f6bc950bb2c85c7c26c550ec85f738a5036221
spiritualsciencezone <spiritualsciencezone@gmail.com>
2026-08-23 17:12:33 +0700

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -32,7 +32,7 @@ export function About() {
 
           <div className="mt-12 overflow-hidden border border-border/60">
           <Image
-          src="/public/images/struktur-sistem-kedirian.png"
+          src="/images/Struktur-Sistem-Kedirian.png"
           alt="Infografis Struktur Sistem Kedirian"
           width={1536}
           height={2048}
